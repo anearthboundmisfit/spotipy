530f5f5832ab709bd205ed7da75737f7f1c6996b
anearthboundmisfit <anearthboundmisfit@users.noreply.github.com>
2022-04-17 22:39:21 +0100

    Interim commit, updating iterator.py
    Remove excessive comments and move constants into class.

--- spotipy/iterator.py
+++ spotipy/iterator.py
@@ -10,58 +10,34 @@ import inflect
 from spotipy import Spotify, SpotifyException
 from spotipy.oauth2 import SpotifyClientCredentials
 
-SEARCH_REGEXP = re.compile('^https?://[^/]+/v1/search\\b')
-SEARCH_MAX_OFFSET = 1000
-
 class SpotipyIterator:
     """Make an iterator to use with Spotify API results"""
 
-    inflector = inflect.engine()
+    INFLECT = inflect.engine()
+    SEARCH_REGEXP = re.compile('^https?://[^/]+/v1/search\\b')
+    SEARCH_MAX_OFFSET = 1000
  
     def __init__(self, *args, **kwargs):
         self._client = None
-        # An instance of the Spotify API client may be passed-in for the,
-        # iterator to use, or one will created automatically.
         if kwargs:
             if 'client' in kwargs:
                 self._client = kwargs['client']
-                # Because we can pass-in a result OR a Spotify API function to
-                # call (and its arguments), to generate such a result, we must
-                # take care to remove any keyword arguments not expected by a
-                # function we might pass-in.
                 del kwargs['client']
         if not self._client:
             self._client = Spotify(auth_manager=SpotifyClientCredentials(), 
                                    requests_session=True)
         self._collection = None
-        # The Spotify API's search endpoint produces a result containing one
-        # or more subcollections depending on the type of records searched
-        # for. We need to be able to identify (or infer) which collection
-        # is to be scanned in the case of a search.
         if kwargs:
             if 'collection' in kwargs:
                 self._collection = kwargs['collection']
-                # Because we can pass-in a result OR a Spotify API function to
-                # call (and its arguments), to generate such a result, we must
-                # take care to remove any keyword arguments not expected by a
-                # function we might pass-in.
                 del kwargs['collection']
             else:
-                # We can infer and inflect from the "type" argument, which
-                # would be present when delegating the creation of the 
-                # result.
                 if 'type' in kwargs:
-                    self._collection = self.inflector.plural(kwargs['type'])
-        # If a function (and its arguments) was passed-in, we should call it
-        # in order to get the results, otherwise we assume a page of results
-        # was passed-in.
+                    self._collection = self.INFLECT.plural(kwargs['type'])
         result, *args = args
         if callable(result):
             result = result(self._client, *args, **kwargs)
         self._page = result
-        # If, at this point, the result isn't recognisable as a page of data,
-        # and the collection type is still unkown, then the collection may be
-        # identified as the name of the result's first key.
         if self._page and 'total' not in self._page:
             if not self._collection:
                 self._collection, *_ = self._page.keys()
@@ -96,7 +72,7 @@ class SpotipyIterator:
         return self
 
     def _search_endpoint_and_offset_limit_reached(self):
-        if SEARCH_REGEXP.match(self._page['next']):
-            if self._page['offset'] + self._page['limit'] >= SEARCH_MAX_OFFSET:
+        if self.SEARCH_REGEXP.match(self._page['next']):
+            if self._page['offset'] + self._page['limit'] >= self.SEARCH_MAX_OFFSET:
                 return True
         return False
